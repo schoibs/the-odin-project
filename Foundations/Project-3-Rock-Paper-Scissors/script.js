0e37c7dca298c9a472d5f4b919c62cace515a353
Choi Bing Shen <bingshen@quickdesk.io>
2022-06-15 22:25:41 +0800

    Implemented the 5-round game function

--- Foundations/Project-3-Rock-Paper-Scissors/script.js
+++ Foundations/Project-3-Rock-Paper-Scissors/script.js
@@ -4,32 +4,50 @@ function computerPlay(){
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-
-// Write a function that plays a single round of Rock Paper Scissors. 
-// The function should take two parameters - the playerSelection and computerSelection - 
-// and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
 function playSingleRound(playerSelection, computerSelection){
 
     const rules = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'};
 
     if (computerSelection === rules[playerSelection]){
         // player lose
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
     } else if (playerSelection === computerSelection){
         // tie
         return "Its a Tie!";
     } else {
         // player win
-        return `You Lose! ${playerSelection} beats ${computerSelection}`;
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
     }
 
 }
 
-console.log(playSingleRound('rock', 'paper'));
+function game(){
 
+    let playerScore = 0;
+    let computerScore = 0;
 
-// Write a function called game(). 
-// Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-// Use prompt() to get input from the user.
+    for (let i=0; i<5; i++){
+
+        console.log(`Round ${i+1}`);
+
+        let playerSelection = prompt("Pick your poison: 'rock', 'paper' or 'scissors'.");
+        let computerSelection = computerPlay();
+
+        let message = playSingleRound(playerSelection, computerSelection);
+        
+        if(message.charAt(4) === 'W'){
+            playerScore++;
+        } else if(message.charAt(4) === 'L'){
+            computerScore++;
+        } else {
+            i--;
+        }
+
+        console.log(`${message} \nPlayer ${playerScore} : ${computerScore} Computer`);
+    }
+
+    const winner = (playerScore > computerScore) ? 'Player Wins!' : 'Computer Wins!';
+    console.log(winner);
+}
+
+game();
