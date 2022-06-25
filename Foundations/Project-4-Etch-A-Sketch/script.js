eec071617f5675b27025daaa5f5dd925dbf80e30
Choi Bing Shen <choibingshen2001@gmail.com>
2022-06-25 11:09:17 +0800

    Implemented proper 16x16 grid with javascript

--- Foundations/Project-4-Etch-A-Sketch/script.js
+++ Foundations/Project-4-Etch-A-Sketch/script.js
@@ -3,11 +3,12 @@ const container = document.querySelector('.container');
 for (let i=0; i<16; i++){
 
     const lineDiv = document.createElement('div');
-    lineDiv.setAttribute('style', 'display: flex; ')
+    lineDiv.setAttribute('style', 'display: flex;')
 
     for (let j=0; j<16; j++){
         const cellDiv = document.createElement('div');
-        cellDiv.textContent = 'cell';
+        cellDiv.setAttribute('style', 'height: 30px; width: 30px; border-style: solid; border-width: 1px;');
+
         lineDiv.appendChild(cellDiv);
     }
 
