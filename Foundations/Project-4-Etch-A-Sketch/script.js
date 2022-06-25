a34ed4c1aeb1dcd1d5fcfd6fb33d9dd5c52f47eb
Choi Bing Shen <choibingshen2001@gmail.com>
2022-06-25 11:13:52 +0800

    Moved css styling of grid cells from script file to css file

--- Foundations/Project-4-Etch-A-Sketch/script.js
+++ Foundations/Project-4-Etch-A-Sketch/script.js
@@ -3,14 +3,15 @@ const container = document.querySelector('.container');
 for (let i=0; i<16; i++){
 
     const lineDiv = document.createElement('div');
-    lineDiv.setAttribute('style', 'display: flex;')
+    lineDiv.classList.add('line');
 
     for (let j=0; j<16; j++){
         const cellDiv = document.createElement('div');
-        cellDiv.setAttribute('style', 'height: 30px; width: 30px; border-style: solid; border-width: 1px;');
-
+        cellDiv.classList.add('cell');
+        
         lineDiv.appendChild(cellDiv);
     }
 
     container.appendChild(lineDiv);
 }
+
