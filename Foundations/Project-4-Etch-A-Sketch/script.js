2acfb033dccca91c77941aaae58cde55ce7d4378
Choi Bing Shen <choibingshen2001@gmail.com>
2022-06-25 11:32:53 +0800

    Implemented background-color change for cell upon mouse hover based on input from color-picker

--- Foundations/Project-4-Etch-A-Sketch/script.js
+++ Foundations/Project-4-Etch-A-Sketch/script.js
@@ -8,10 +8,19 @@ for (let i=0; i<16; i++){
     for (let j=0; j<16; j++){
         const cellDiv = document.createElement('div');
         cellDiv.classList.add('cell');
-        
+
         lineDiv.appendChild(cellDiv);
     }
 
     container.appendChild(lineDiv);
 }
+
+const colorInput = document.querySelector('input');
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell) => {
+    cell.addEventListener('mouseenter', e => {
+        cell.style.backgroundColor = colorInput.value;
+    });
+});
 
