032597765ae8e4c6f88d80cf3f180c13ab82a167
Choi Bing Shen <choibingshen2001@gmail.com>
2022-06-28 21:05:22 +0800

    Implemented reset button logic

--- Foundations/Project-4-Etch-A-Sketch/script.js
+++ Foundations/Project-4-Etch-A-Sketch/script.js
@@ -20,7 +20,26 @@ const cells = document.querySelectorAll('.cell');
 
 cells.forEach((cell) => {
     cell.addEventListener('mouseenter', e => {
-        cell.style.backgroundColor = colorInput.value;
+        e.target.style.backgroundColor = colorInput.value;
     });
 });
 
+
+const eraserBtn = document.querySelector('.eraser-btn');
+const rainbowBtn = document.querySelector('.rainbow-btn');
+const resetBtn = document.querySelector('.reset-btn');
+
+eraserBtn.addEventListener('click', e => {
+    e.target.style.backgroundColor = 'rgb(255, 168, 168)';
+
+});
+
+rainbowBtn.addEventListener('click', e => {
+    
+});
+
+resetBtn.addEventListener('click', e => {
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
+});
