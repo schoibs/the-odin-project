cd110e3d6f09eb84600afa28f891ce1793917227
Choi Bing Shen <choibingshen2001@gmail.com>
2022-07-14 22:16:17 +0800

    Implement functions for basic maths operations

---
+++ Foundations/Project-5-Calculator/script.js
@@ -0,0 +1,38 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operation, num1, num2){
+    
+    let ans = 0;
+    
+    switch(operation){
+        case 'add':
+            ans = add(num1, num2);
+            break;
+        case 'subtract':
+            ans = subtract(num1, num2);
+            break;
+        case 'multiply':
+            ans = multiply(num1, num2);
+            break;
+        case 'divide':
+            ans = divide(num1, num2);
+            break;
+    }
+
+    return ans;
+    
+}
