cca2e8b52c2c24107525351ebfd6bcd09820f30a
Choi Bing Shen <choibingshen2001@gmail.com>
2022-07-16 11:38:07 +0800

    Implement event listeners for all buttons

--- Foundations/Project-5-Calculator/script.js
+++ Foundations/Project-5-Calculator/script.js
@@ -36,3 +36,58 @@ function operate(operation, num1, num2){
     return ans;
     
 }
+
+let firstOperand = 0;
+let operation = '';
+let secondOperand = 0;
+let listeningFor = 'first';
+
+
+const numberButtons = document.querySelectorAll('.number-btn');
+numberButtons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+
+        if (listeningFor === 'first'){
+            firstOperand = +button.textContent;
+        } else if (listeningFor === 'second'){
+            secondOperand = +button.textContent;
+        }
+
+    });
+});
+
+
+const opeartionButtons = document.querySelectorAll('.operation-btn');
+opeartionButtons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        if (button.textContent === '+'){
+            operation = 'add';
+        } else if (button.textContent === '-'){
+            operation = 'subtract';
+        } else if (button.textContent === 'x'){
+            operation = 'multiply';
+        } else if (button.textContent === '/'){
+            operation = 'divide';
+        }
+
+        listeningFor = 'second';
+    });
+});
+
+
+const equalButton = document.querySelector('#equal-btn');
+equalButton.addEventListener('click', () => {
+    console.log(firstOperand, operation, secondOperand);
+    listeningFor = 'first';
+});
+
+
+const clearButton = document.querySelector('#clear-btn');
+equalButton.addEventListener('click', () => {
+    firstOperand = 0;
+    operation = '';
+    secondOperand = 0;
+    listeningFor = 'first';
+});
