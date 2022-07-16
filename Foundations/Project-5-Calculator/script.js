554cb42ee13e2154de04363853ae95830b7e549a
Choi Bing Shen <choibingshen2001@gmail.com>
2022-07-16 12:51:48 +0800

    Allow users to directly do maths operations on result

--- Foundations/Project-5-Calculator/script.js
+++ Foundations/Project-5-Calculator/script.js
@@ -30,44 +30,40 @@ function operate(operation, num1, num2){
             break;
         case 'divide':
             ans = divide(num1, num2);
+            if (ans === Infinity)
+                return "Can't divide by zero, idiot!"
             break;
     }
 
-    return ans;
-    
+    // round to two decimal places if necessary
+    return Math.round((ans + Number.EPSILON) * 100) / 100; 
 }
 
 
+const displayArea = document.querySelector('.display');
+
+// reset the application
 let firstOperand = 0;
 let operation = '';
 let secondOperand = 0;
-let listeningFor = 'first';
-
-
-const displayArea = document.querySelector('.display');
 displayArea.textContent = firstOperand;
 
 const numberButtons = document.querySelectorAll('.number-btn');
 numberButtons.forEach((button) => {
-    // and for each one we add a 'click' listener
+
     button.addEventListener('click', () => {
-
-        if (listeningFor === 'first'){
-            firstOperand = +button.textContent;
-            displayArea.textContent = firstOperand;
-        } else if (listeningFor === 'second'){
-            secondOperand = +button.textContent;
-            displayArea.textContent = secondOperand;
-        }
-
+        displayArea.textContent = +button.textContent;
     });
+
 });
 
 
 const opeartionButtons = document.querySelectorAll('.operation-btn');
 opeartionButtons.forEach((button) => {
-    // and for each one we add a 'click' listener
+
     button.addEventListener('click', () => {
+        firstOperand = +displayArea.textContent;
+
         if (button.textContent === '+'){
             operation = 'add';
         } else if (button.textContent === '-'){
@@ -77,25 +73,26 @@ opeartionButtons.forEach((button) => {
         } else if (button.textContent === '/'){
             operation = 'divide';
         }
-
-        listeningFor = 'second';
     });
+
 });
 
 
 const equalButton = document.querySelector('#equal-btn');
 equalButton.addEventListener('click', () => {
-    let answer = operate(operation, firstOperand, secondOperand);
+
+    secondOperand = +displayArea.textContent;
+
+    const answer = operate(operation, firstOperand, secondOperand);
     displayArea.textContent = answer;
-    listeningFor = 'first';
 });
 
 
 const clearButton = document.querySelector('#clear-btn');
 clearButton.addEventListener('click', () => {
+    // reset the application
     firstOperand = 0;
     operation = '';
     secondOperand = 0;
-    listeningFor = 'first';
     displayArea.textContent = firstOperand;
 });
